fabd99ddddd9065e64cbc69d20b82886447b76ad
Carlos Daniel Maldonado Muñoz <201G0266@rcarbonifera.tecnm.mx>
2023-10-19 19:57:21 -0600

    racgjaksdjf

--- Kirby_New_Adventure/Images.cs
+++ Kirby_New_Adventure/Images.cs
@@ -13,7 +13,7 @@ namespace Kirby_New_Adventure
         public readonly static ImageSource Empty = LoadImages("Empty.png");
         public readonly static ImageSource Tierra = LoadImages("Pasto1.png");
         public readonly static ImageSource Kirby = LoadImages("a.png");
-        public readonly static ImageSource Food = LoadImages("pollo.png");
+        public readonly static ImageSource Food = LoadImages("Comida/Cereza.png");
         public readonly static ImageSource Roca = LoadImages("Roca22.png");
         public readonly static ImageSource Vacio = LoadImages("Hoyo22.png");
         public readonly static ImageSource Estrella = LoadImages("Estrella.png");
